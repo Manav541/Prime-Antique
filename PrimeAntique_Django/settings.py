dd5920e1e696c112cbe7f714ab5e6e86cd7f8496
Manav Mistry <137400266+Manav541@users.noreply.github.com>
2023-08-20 14:51:54 +0530

    Update settings.py

--- PrimeAntique_Django/settings.py
+++ PrimeAntique_Django/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manavmistry542001@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mr__mnv541'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
